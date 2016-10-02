3792a0d3fb3c6a9dff39cc75bd18403d16e4a7f9
Lasse <lasse@dyrsted.dk>
2016-10-02 18:59:03 +0200

    Change Package name
    Changed package name and changed code to fit specifications.

--- src/game/Dice.java
+++ src/game/Dice.java
@@ -1,4 +1,4 @@
-package spil;
+package game;
 
 import java.util.Random;
 
@@ -6,13 +6,8 @@ import java.util.Random;
  * Created by s165240 on 23-09-2016.
  */
 public class Dice {
-    private int faceValue;
+    private int faceValue = 1;
     private Random rand = new Random();
-
-
-    public Dice() {
-        faceValue = rand.nextInt(6) + 1;
-    }
 
     public void Roll() {
         faceValue = rand.nextInt(6) + 1;
